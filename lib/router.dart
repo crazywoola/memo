34581f65acf25aaa45e86dcbe2ab5732d85cb909
crazywoola <li.zheng@dentsplysirona.com>
2022-08-29 13:28:09 +0800

    Add: book screen

--- lib/router.dart
+++ lib/router.dart
@@ -1,27 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:beamer/beamer.dart';
 import './widgets/home_screen.dart';
+import './widgets/books_screen.dart';
+import './widgets/book_detail_screen.dart';
+import './data.dart';
 
 final simpleLocationBuilder = RoutesLocationBuilder(routes: {
   '/': (context, state, data) => BeamPage(
-        key: ValueKey('home'),
+        key: const ValueKey('home'),
         title: 'Home',
         child: HomeScreen(),
       ),
-  // '/books': (context, state, data) => BeamPage(
-  //       key: ValueKey('books'),
-  //       title: 'Books',
-  //       child: BooksScreen(),
-  //     ),
-  // '/books/:bookId': (context, state, data) {
-  //   final book = books.firstWhere((book) =>
-  //       book['id'] ==
-  //       (context.currentBeamLocation.state as BeamState)
-  //           .pathParameters['bookId']);
+  '/books': (context, state, data) => BeamPage(
+        key: const ValueKey('books'),
+        title: 'Books',
+        child: BooksScreen(),
+      ),
+  '/books/:bookId': (context, state, data) {
+    final book = books.firstWhere((book) =>
+        book['id'] ==
+        (context.currentBeamLocation.state as BeamState)
+            .pathParameters['bookId']);
 
-  //   return BeamPage(
-  //     key: ValueKey('book-${book['id']}'),
-  //     title: book['title'],
-  //     child: BookDetailsScreen(book: book),
-  //   );
+    return BeamPage(
+      key: ValueKey('book-${book['id']}'),
+      title: book['title'],
+      child: BookDetailScreen(book: book),
+    );
+  }
 });
---
+++ lib/widgets/books_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:beamer/beamer.dart';
+import '../data.dart';
+
+class BooksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Books'),
+      ),
+      body: ListView(
+        children: books
+            .map(
+              (book) => ListTile(
+                title: Text(book['title']!),
+                subtitle: Text(book['author']!),
+                onTap: () => context.beamToNamed('/books/${book['id']}'),
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
